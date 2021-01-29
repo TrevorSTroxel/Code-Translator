356915e6e5dc5521dbf3273907907938031e8e4b
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-01-28 16:38:38 -0800

    Got method to work

--- Panel_Window_Methods.java
+++ Panel_Window_Methods.java
@@ -8,29 +8,7 @@ import java.awt.event.ActionListener;
 public class Panel_Window_Methods implements ActionListener {
 	// This file will store where out panels settings are stored
 	TR_Variables TRV = new TR_Variables();
-
-	public void Input_Window(JPanel JP) //work on gettign JP to work correctly
-	{
-		TRV.Input_Window.getContentPane().removeAll();//This is to make sure that the wondow is reuseable every time
-		TRV.Input_Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		TRV.Input_Window.setSize(500, 500);
-		TRV.Input_Window.add(JP); // find out why JP does not work
-		TRV.Input_Window.getContentPane().validate();
-		TRV.Input_Window.repaint();
-		TRV.Input_Window.setVisible(true);
-	}
-
-	public void test() //testing ideas
-	{
-		TRV.Input_Window.getContentPane().removeAll();//This is to make sure that the wondow is reuseable every time
-		TRV.Input_Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		TRV.Input_Window.setSize(500, 500);
-		TRV.Input_Window.add(TRV.Folder_Panel); // find out why JP does not work
-		TRV.Input_Window.getContentPane().validate();
-		TRV.Input_Window.repaint();
-		TRV.Input_Window.setVisible(true);
-	}
-
+	
 	/**
 	 * The only purpose of this method is to run all my Panel settings methods.
 	 * We could not find a more efficient way of adding content to my panels in a effectively.
@@ -39,6 +17,27 @@ public class Panel_Window_Methods implements ActionListener {
 	public void Method_Runner() 
 	{
 		File_Creation_Panel();
+	}
+
+
+	//JP is considerend its own, seperate JPanel and not the input the method call
+	public void Input_Window(JPanel JP) //we have to grab the name of the panel input to set it correctly
+	{
+		TRV.Input_Window.getContentPane().removeAll();//This is to make sure that the wondow is reuseable every time
+		TRV.Input_Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		TRV.Input_Window.setSize(500, 500);
+		
+		//We will have to do a few different loops once we add more panels
+		//this is the only way to get different panels added to the frame
+		if (JP.getName() == TRV.Folder_Panel.getName()) 
+		{
+			TRV.Input_Window.add(TRV.Folder_Panel); // find out why JP does not work //turns out you need to manually set the manels
+		}
+		
+		TRV.Input_Window.getContentPane().validate();
+		TRV.Input_Window.repaint();
+		TRV.Input_Window.setVisible(true);
+
 	}
 
 	public void File_Creation_Panel() 
@@ -52,7 +51,23 @@ public class Panel_Window_Methods implements ActionListener {
 
 		TRV.File_Name_Button.addActionListener(this);
 		TRV.Folder_Path_Button.addActionListener(this);
+
+		TRV.Folder_Panel.setEnabled(true);
 	}
+
+	// //dumb idea but i wat to test it
+	// public void testing_something()
+	// {
+	// 	JP.setLayout(new GridLayout(2,2));
+	// 	JP.add(TRV.Folder_Path_Button);
+	// 	JP.add(TRV.Folder_Path_Text_Area);
+
+	// 	JP.add(TRV.File_Name_Button);
+	// 	JP.add(TRV.File_Name_Text_Area);
+
+	// 	TRV.File_Name_Button.addActionListener(this);
+	// 	TRV.Folder_Path_Button.addActionListener(this);
+	// }
 
 	@Override
 	public void actionPerformed(ActionEvent e) 
