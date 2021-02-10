e2e2b7a485ad5e97bf7a208a2e70d54b080795d0
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-02-09 16:41:07 -0800

    Shortened possible inputs for easier coding

--- Button_Clicked.java
+++ Button_Clicked.java
@@ -15,64 +15,50 @@ public class Button_Clicked implements ActionListener
     @Override
     public void actionPerformed(ActionEvent e) 
     {
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		//Create file panel buttons
-        if (e.getSource() == Variables.Folder_Path_Button) 
+		/**
+		 * These new generics work correctly, and my other paramaters are also working as intended
+		 * next step is to basically just have generics in my variable file.
+		 * Reason why we do that is because it is 
+		 * 1. easier to program and to help clear up some coding space for us
+		 * 2. gives us less confusion on the naming scheme for a lot of these parmaters
+		 * 3. When we actually need to have a unique buttons and jtextareas, it will be a lot more noticable for us
+		 */
+		if (e.getSource() == Variables.Path_Button)
 		{
-			Generic_Methods.Location(Variables.Folder_Path_Text_Area, 1); //grabs the text in the folder path area
-			Variables.Paramater1 = Variables.Folder_Path_Text_Area.getText(); //sets the generic paramater1 to hold the folder location
-		} 
-		else if (e.getSource() == Variables.File_Name_Button) 
-		{
-			Variables.Paramater2 = Variables.File_Name_Text_Area.getText(); //sets the text in the file name area to the generic paramater2
-			Create.Create_File(Variables.Paramater1, Variables.Paramater2); //runs our file creation method using our paramaters
-			Generic_Methods.Remove_Button_Enabler(1);
+			if (Variables.Folder_Panel.isDisplayable() == true)
+			{
+				Generic_Methods.Location(Variables.Text_Area_Paramater1, 1); //grabs the text in the folder path area
+				Variables.Paramater1 = Variables.Text_Area_Paramater1.getText(); //sets the generic paramater1 to hold the folder location
+			}
+			else
+			{
+				Generic_Methods.Location(Variables.Text_Area_Paramater1, 2); //grabs the text in the folder path area
+				Variables.Paramater1 = Variables.Text_Area_Paramater1.getText(); //sets the generic paramater1 to hold the folder location
+			}
 		}
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		else if (e.getSource() == Variables.File_Button)
+		else if (e.getSource() == Variables.Naming_Button1)
 		{
-			Generic_Methods.Location(Variables.File_Text_Area, 2);
-			Variables.Paramater1 = Variables.File_Text_Area.getText();
+			Variables.Paramater2 = Variables.Text_Area_Paramater2.getText();
+			if (Variables.Folder_Panel.isDisplayable() == true)
+			{
+				Create.Create_File(Variables.Paramater1, Variables.Paramater2); //runs our file creation method using our paramaters
+				Generic_Methods.Remove_Button_Enabler(1);
+			}
 		}
-		else if (e.getSource() == Variables.Return_Type_Button)
+		else if (e.getSource() == Variables.Naming_Button2)
 		{
-			Variables.Paramater2 = Variables.Return_Type_Text_Area.getText();
-		}
-		else if (e.getSource() == Variables.Method_Name_Button)
-		{
-			Variables.Paramater3 = Variables.Method_Name_Text_Area.getText();
-			Create.Create_Method(Variables.Paramater1, Variables.Paramater2, Variables.Paramater3);
-			Generic_Methods.Remove_Button_Enabler(1);
-		}
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		//will com back to, but first I want to add the functionality to add methods
-		//add content panel buttons
-		else if (e.getSource() == Variables.File_Path_Button)
-		{
-			Generic_Methods.Location(Variables.File_Path_Text_Area, 2); //grabs the text in the folder path area
-			Variables.Paramater1 = Variables.File_Path_Text_Area.getText(); //sets the generic paramater1 to hold the folder location
-		}
-		else if (e.getSource() == Variables.Getting_Method_Name_Button)
-		{
-			Variables.Paramater2 = Variables.Getting_Method_Name_Text_Area.getText();
-		}
-		else if (e.getSource() == Variables.File_Add_Content_Button)
-		{
-			Variables.Paramater3 = Variables.File_Add_Content_Text_Area.getText();
-			Create.Add_To_Method(Variables.Paramater1, Variables.Paramater2, Variables.Paramater3);
-			Generic_Methods.Remove_Button_Enabler(1);
-		}
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			Variables.Paramater3 = Variables.Text_Area_Paramater3.getText();
 
-		
+			if (Variables.Method_Panel.isDisplayable() == true)
+			{
+				Create.Create_Method(Variables.Paramater1, Variables.Paramater2, Variables.Paramater3);
+			}
+			else if (Variables.Content_Panel.isDisplayable() == true)
+			{
+				Create.Add_To_Method(Variables.Paramater1, Variables.Paramater2, Variables.Paramater3);
+			}
+			Generic_Methods.Remove_Button_Enabler(1);
+		}
 	}
 
     
