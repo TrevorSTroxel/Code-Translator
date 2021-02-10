350132b37aeb542fc5f48409bb88f6bc5011d333
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-02-09 16:42:56 -0800

    Changed almost all methods to generics

--- Variables.java
+++ Variables.java
@@ -26,55 +26,38 @@ public class Variables
     public static JButton TR_Enter = new JButton("Enter");
     public static String User_Input = new String();
     //testing variables for the first planel
-    public static JButton test = new JButton("testing help file");
+    public static JButton Help = new JButton("Click if you do not know what to do");
     public static JTextArea Instructions = new JTextArea(20, 20);
     public static JPanel Help_Panel = new JPanel();
     ////////////////////////////////////////////////////////////////////////
     public static Queue<String> Order_Of_Commands = new LinkedList<>(); //used to store all the commands that the user has put into it
+    public static JFrame Input_Window = new JFrame(); //The secondary window that is reused several times
+    public static JButton Remove = new JButton("Click this button to move onto the next command"); //pressing this will remove the top element from the queue to move to the next command
     ////////////////////////////////////////////////////////////////////////
     // generic variables that we call on to re-use at anytime
+    //File location button to grab the location
+    public static JButton Path_Button = new JButton("Click this to select your folder");
+    //This is for when we ask the user to name something, will make a method to change the text later
+    public static JButton Naming_Button1 = new JButton("Click this once you have entered a name 1");
+    public static JButton Naming_Button2 = new JButton("Click this once you have entered a name 2");
+
+    //Where the users inputs for the different text fields will go
+    public static JTextArea Text_Area_Paramater1 = new JTextArea(1,20);
+    public static JTextArea Text_Area_Paramater2 = new JTextArea(1,20);
+    public static JTextArea Text_Area_Paramater3 = new JTextArea(1,20);
+
+    //something to store the text results in
     public static String Paramater1 = new String();
     public static String Paramater2 = new String();
     public static String Paramater3 = new String();
     ////////////////////////////////////////////////////////////////////////
-    public static JFrame Input_Window = new JFrame(); //The secondary window that is reused several times
-    public static JButton Remove = new JButton("Click this button to move onto the next command"); //pressing this will remove the top element from the queue to move to the next command
-    ////////////////////////////////////////////////////////////////////////
+    //this section is where we will have the different panels
+    //may think about making a method where it also 
+    //creats a bunch of generic variabels depending on the queue size, but for now it is fine
     //File creation variables
     public static JPanel Folder_Panel = new JPanel();
-
-    public static JButton Folder_Path_Button = new JButton("Click to select the folder where you want to create a file");
-    public static JTextArea Folder_Path_Text_Area = new JTextArea(1, 20);
-
-    public static JButton File_Name_Button = new JButton("Click this button once you have typed what you want to name the file");
-    public static JTextArea File_Name_Text_Area = new JTextArea(1, 20);
-    ////////////////////////////////////////////////////////////////////////
     //adding methods to the file variable
     public static JPanel Method_Panel = new JPanel();
-
-    public static JButton File_Button = new JButton("Click to select the folder where you want to create a file");
-    public static JTextArea File_Text_Area = new JTextArea(1, 20);
-
-    public static JButton Return_Type_Button = new JButton("Enter the return type of your method");
-    public static JTextArea Return_Type_Text_Area = new JTextArea(1, 20);
-
-    public static JButton Method_Name_Button = new JButton("Enter what you want to name your method");
-    public static JTextArea Method_Name_Text_Area = new JTextArea(1, 20);
-
-    ////////////////////////////////////////////////////////////////////////
-    //Adding content variables
+    //Adding content panel
     public static JPanel Content_Panel = new JPanel();
-
-    //what file to add it to
-    public static JButton File_Path_Button = new JButton("Click to select the file you want to add content to");
-    public static JTextArea File_Path_Text_Area = new JTextArea(1, 20);
-
-    public static JButton Getting_Method_Name_Button = new JButton("What is the name of the method you  want to add to?");
-    public static JTextArea Getting_Method_Name_Text_Area = new JTextArea(1, 20);
-    
-    public static JButton File_Add_Content_Button = new JButton("Click this button once you have typed what you want the program to say");
-    public static JTextArea File_Add_Content_Text_Area = new JTextArea(1, 20);
-    ////////////////////////////////////////////////////////////////////////
-    // testing variables
-    public static JPanel File_Naming_test_EX = new JPanel();
 }
